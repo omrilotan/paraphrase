5507ab9f64b345b581485e90309ac3e6150a083c
omrilotan <omri@hamadgera.com>
2021-01-08 23:41:05 +0200

    Internal: Move to parcel bundler

---
+++ scripts/build.sh
@@ -0,0 +1,6 @@
+parcel build "src/dollar/index.js" --out-dir dollar
+parcel build "src/double/index.js" --out-dir double
+parcel build "src/hash/index.js" --out-dir hash
+parcel build "src/percent/index.js" --out-dir percent
+parcel build "src/single/index.js" --out-dir single
+parcel build "src/index.js" --out-dir .
